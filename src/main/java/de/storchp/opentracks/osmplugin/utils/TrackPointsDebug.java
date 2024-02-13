91f54d15e30ab0944babaf657249c3dda685559b
harsh mithaiwala <harsh.mithaiwala.hm@gmail.com>
2024-02-12 23:56:06 -0500

    Class Constructor - 40221295

--- src/main/java/de/storchp/opentracks/osmplugin/utils/TrackPointsDebug.java
+++ src/main/java/de/storchp/opentracks/osmplugin/utils/TrackPointsDebug.java
@@ -1,22 +1,14 @@
 package de.storchp.opentracks.osmplugin.utils;
 
 public class TrackPointsDebug {
-    private int trackpointsReceived = 0;
+    public int trackpointsReceived = 0;
     public int trackpointsInvalid = 0;
     public int trackpointsDrawn = 0;
     public int trackpointsPause = 0;
     public int segments = 0;
 
-    public int getTrackpointsReceived() {
-        return trackpointsReceived;
-    }
-
-    public void setTrackpointsReceived(int trackpointsReceived) {
-        this.trackpointsReceived = trackpointsReceived;
-    }
-
     public void add(final TrackPointsDebug other) {
-        this.trackpointsReceived.setTrackpointsReceived(this.trackpointsReceived + other.trackpointsReceived());
+        this.trackpointsReceived += other.trackpointsReceived;
         this.trackpointsInvalid += other.trackpointsInvalid;
         this.trackpointsDrawn += other.trackpointsDrawn;
         this.trackpointsPause += other.trackpointsPause;
