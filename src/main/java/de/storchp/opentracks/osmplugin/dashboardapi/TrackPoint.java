ea245e74cf196211b1db109aafb708dc5b8b7e4e
HWJFish <wwc641810324@gmail.com>
2024-02-15 21:31:27 -0500

    Revert "Merge branch 'c1_deliverable_KRT'"
    This reverts commit 9232670abaa82ce8358edfb06305a5ea17f8dfaf, reversing
    changes made to 881852049704e0ec61ff3b2e56a255dbdb87918e.

--- src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/TrackPoint.java
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/TrackPoint.java
@@ -114,18 +114,10 @@ public class TrackPoint {
                 if (lastTrackPoint.isPause()) {
                     debug.trackpointsPause++;
                     if (!lastTrackPoint.hasValidLocation()) {
-                       if (!segment.isEmpty()) {
+                       if (segment.size() > 0) {
                            var previousTrackpoint = segment.get(segment.size() - 1);
                            if (previousTrackpoint.hasValidLocation()) {
-                               segment.add(
-                                       new TrackPoint(
-                                       trackId,
-                                       trackPointId,
-                                       previousTrackpoint.getLatLong().getLatitude(),
-                                       previousTrackpoint.getLatLong().getLongitude(),
-                                       type,
-                                       speed)
-                               );
+                               segment.add(new TrackPoint(trackId, trackPointId, previousTrackpoint.getLatLong().getLatitude(), previousTrackpoint.getLatLong().getLongitude(), type, speed));
                            }
                        }
                     }
