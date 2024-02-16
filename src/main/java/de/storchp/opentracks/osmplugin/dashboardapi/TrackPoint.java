bf2d5feb19b2764fa4eb2959ed3b58809ef0b3cc
Priscila Aramouni <priscila.aramouni@gmail.com>
2024-02-15 21:18:25 -0500

    Merge nested "if" statements #7

--- src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/TrackPoint.java
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/TrackPoint.java
@@ -113,12 +113,10 @@ public class TrackPoint {
                 }
                 if (lastTrackPoint.isPause()) {
                     debug.trackpointsPause++;
-                    if (!lastTrackPoint.hasValidLocation()) {
-                       if (segment.size() > 0) {
-                           var previousTrackpoint = segment.get(segment.size() - 1);
-                           if (previousTrackpoint.hasValidLocation()) {
-                               segment.add(new TrackPoint(trackId, trackPointId, previousTrackpoint.getLatLong().getLatitude(), previousTrackpoint.getLatLong().getLongitude(), type, speed));
-                           }
+                    if (!lastTrackPoint.hasValidLocation() && segment.size() > 0) {
+                       var previousTrackpoint = segment.get(segment.size() - 1);
+                       if (previousTrackpoint.hasValidLocation()) {
+                           segment.add(new TrackPoint(trackId, trackPointId, previousTrackpoint.getLatLong().getLatitude(), previousTrackpoint.getLatLong().getLongitude(), type, speed));
                        }
                     }
                     lastTrackPoint = null;
