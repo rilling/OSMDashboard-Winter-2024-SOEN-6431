add02a3ede088f87197071a045a7656e0bd7f09a
Shrey Agrawal <68065864+Shrey1002@users.noreply.github.com>
2024-03-23 15:34:32 -0400

    added line graph

--- src/main/java/de/storchp/opentracks/osmplugin/option4.java
+++ src/main/java/de/storchp/opentracks/osmplugin/option4.java
@@ -1,19 +1,51 @@
 package de.storchp.opentracks.osmplugin;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class option4 extends MapsActivity{
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class option4 extends MapsActivity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option4);
 
-        // Find the TextView in the layout
-        TextView textViewHelloWorld = findViewById(R.id.option4);
+        // Find the LineChart in the layout
+        LineChart lineChart = findViewById(R.id.lineChart);
 
-        // Set the text to "Hello World!"
-        textViewHelloWorld.setText("Hello World!");
+        // Create sample data for the line chart
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 10));
+        entries.add(new Entry(2, 20));
+        entries.add(new Entry(3, 15));
+        entries.add(new Entry(4, 25));
+        entries.add(new Entry(5, 30));
+
+        // Create a LineDataSet with the sample data
+        LineDataSet dataSet = new LineDataSet(entries, "Sample Data");
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK);
+
+        // Create a LineData object with the LineDataSet
+        LineData lineData = new LineData(dataSet);
+
+        // Set the LineData to the LineChart
+        lineChart.setData(lineData);
+
+        // Customize the LineChart as needed
+        Description description = new Description();
+        description.setText("Sample Line Chart");
+        lineChart.setDescription(description);
+        lineChart.animateXY(1000, 1000);
+        lineChart.invalidate(); // Refresh the chart
     }
-
 }
